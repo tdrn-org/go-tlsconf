421e671b6cdd7a836012370fe239b6786fb8bfd3
Holger <hdecarne@gmail.com>
2026-01-10 05:14:53 +0100

    Update copyright string

--- tlsclient/tlsclient_test.go
+++ tlsclient/tlsclient_test.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2025 Holger de Carne
+// Copyright (C) 2025-2026 Holger de Carne
 //
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
